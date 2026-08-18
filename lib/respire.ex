3d76dcd1d4fb8d819bdfbf0e8087d53474328b04
mathews lima <mathews.limaa@gmail.com>
2026-08-17 22:11:01 -0300

    Update module documentation for Respire to clarify functionality

--- lib/respire.ex
+++ lib/respire.ex
@@ -1,5 +1,8 @@
 defmodule Respire do
   @moduledoc """
-  Documentation for `Respire`.
+  A Redis-compatible server built on plain OTP.
+
+  Speaks RESP2 over TCP and implements a subset of the Redis command set,
+  using only the standard library.
   """
 end
