1a47329011dcff386b3758464d3ca620b0f955f2
mathews lima <mathews.limaa@gmail.com>
2026-08-17 22:48:16 -0300

    Implement encode functions for RESP module to handle various data types

--- lib/respire/resp.ex
+++ lib/respire/resp.ex
@@ -1,2 +1,23 @@
 defmodule Respire.RESP do
+  def encode({:simple, text}) do
+    "+" <> text <> "\r\n"
+  end
+
+  def encode({:error, text}) do
+    "-" <> text <> "\r\n"
+  end
+
+  def encode({:integer, number}) do
+    ":" <> Integer.to_string(number) <> "\r\n"
+  end
+
+  def encode({:bulk, text}) do
+    size = byte_size(text)
+
+    "$" <> Integer.to_string(size) <> "\r\n" <> text <> "\r\n"
+  end
+
+  def encode(:null) do
+    "$-1\r\n"
+  end
 end
